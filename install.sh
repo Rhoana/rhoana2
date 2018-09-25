c4d0d6973dcd327c7eb9f94b01f78c3e5efc1562
matinraayai <raayaiardakani.m@husky.neu.edu>
2018-09-25 13:57:53 -0400

    Added requirement install.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ function install_pkg {
     echo "__path__ = __import__('pkgutil').extend_path(__path__, __name__)" > \
     ./src/$1/rh2/__init__.py;
     cd ./src/$1/;
-	pip install .;
+	pip install -r ./rh2/requirements.txt .;
 	cd ../..;
 }
 
