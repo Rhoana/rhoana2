a72c1bc001b63dd7825b9766eca8fdc64a6a0a72
donglaiw <donglai@rclogin05.rc.fas.harvard.edu>
2018-11-27 17:53:45 -0500

    remove build/egg folder

--- src/zwatershed/build/lib.linux-x86_64-2.7/rh2/zwatershed/__init__.py
+++
@@ -1,7 +0,0 @@
-from mean_aff import mean_agglomeration, ma_get_region_graph, ma_merge_region
-from .zwatershed import zwatershed, zw_initial, zw_merge_region
-
-# for debug
-from .zwatershed import zw_find_basins,zw_divide_plateaus,zw_steepest_ascent
-from .zwatershed import zw_get_region_graph, zw_get_region_graph_average
-from .zwatershed import zw_merge_segments_with_function, zw_do_mapping
